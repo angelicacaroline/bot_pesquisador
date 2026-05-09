a3d52f00fc546a4f84a2700df308e79958a52262
Angelica <angelicacaroline1995@gmail.com>
2026-05-09 00:26:49 -0300

    🚧 em desenvolvimento: ainda possui erros, utilizado enciclopedia

--- bot.py
+++ bot.py
@@ -3,21 +3,39 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import time
 
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
 servico = Service(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=servico)
+navegador = webdriver.Chrome(service=servico, options=chrome_options)
 
 try:
-    navegador.get("https://www.google.com")
-    campo_busca = navegador.find_element(By.NAME, "q")
-    campo_busca.send_keys("qual a temperatura de hoje em São Paulo")
+    #navegador.get("https://www.google.com")
+    navegador.get("https://www.wikipedia.org/")
+    
+    campo_busca = navegador.find_element(By.NAME, "search")
+    campo_busca.send_keys("Inteligencia Artificial")
     campo_busca.send_keys(Keys.ENTER)
-    time.sleep(5000)  # Aguarda a página carregar completamente
-    resultado = navegador.find_element((By.CSS_SELECTOR, 'span[data-value]')).get_attribute("data-value")
-    print(f"O bot encontrou o valor: {resultado}")
+    
+    navegador.execute_script("window.scrollBy(0, 1000);")
+    time.sleep(2)  # Aguarda a página carregar completamente
+    
+    #resultado = navegador.find_element((By.CSS_SELECTOR, 'span[data-value]')).get_attribute("data-value")
+    
+    #link_teste = WebDriverWait(navegador, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Aprendizado de máquina")))
+    link_teste = WebDriverWait(navegador, 10).until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, "A_forte_e_IA_fraca")))
+
+    
+    link_teste.click()
+    
+    time.sleep(4)  # Aguarda a página carregar completamente
     
 except Exception as e:
     print(f"Ocorreu um erro: {e}")
-finally:
-    navegador.quit()
+#finally:
+#    navegador.quit()
